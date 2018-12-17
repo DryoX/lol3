2e7849eab8b8059c5303a3bac518db9efb54eede
DryoX <40540721+DryoX@users.noreply.github.com>
2018-12-17 15:32:01 +0800

    Update test.js

--- commands/test.js
+++ commands/test.js
@@ -2,7 +2,7 @@ const Discord = require("discord.js");
 
 module.exports.run = async (bot,message,args) => {
 
-return message.channel.send(`--everyone@everyone`);
+return message.channel.send(`--everyone @everyone`);
 
 }
 module.exports.help = {

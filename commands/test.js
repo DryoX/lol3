375aa49d28309d28e3c80af878fa2ea8d39d9257
DryoX <40540721+DryoX@users.noreply.github.com>
2018-12-25 10:24:29 +0800

    Update test.js

--- commands/test.js
+++ commands/test.js
@@ -2,7 +2,11 @@ const Discord = require("discord.js");
 
 module.exports.run = async (bot,message,args) => {
 
-return message.channel.send(`--everyone @everyone`);
+let atest = new Discord.RichEmbed()
+   .setColor("RANDOM")
+   .addField("**Information**", "**This is a test command!**");
+
+return message.channel.send(atest).then(message => {message.delete(5000)});
 
 }
 module.exports.help = {

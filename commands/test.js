8e33c6fb57c4536520f837ac8664212a42ec36f7
DryoX <40540721+DryoX@users.noreply.github.com>
2018-12-28 19:40:17 +0800

    Update test.js

--- commands/test.js
+++ commands/test.js
@@ -2,11 +2,14 @@ const Discord = require("discord.js");
 
 module.exports.run = async (bot,message,args) => {
 
-let atest = new Discord.RichEmbed()
-   .setColor("RANDOM")
-   .addField("**Information**", "**This is a test command!**");
+let reason = args.join(" ");
+let user = `$<@{message.author.id}>`;
 
-return message.channel.send(atest).then(message => {message.delete(5000)});
+return message.channel.send("Okay you are now afk!").then(message => {message.delete(5000)});
+
+if(user) return message.channel.send(`That person is afk and the reason is` + reason);
+
+if(message.author.id) return message.channel.send("You are now leaving afk room!");
 
 }
 module.exports.help = {

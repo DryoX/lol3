0a04aa439514c9814c9af6782a61e4dc9bb9aa2b
DryoX <40540721+DryoX@users.noreply.github.com>
2019-06-26 02:59:58 +0800

    Update massclear.js

--- commands/massclear.js
+++ commands/massclear.js
@@ -4,7 +4,7 @@ module.exports.run = async (bot, message, args) => {
 
 
 if(!message.member.hasPermission("MANAGE_MESSAGES")) return message.reply("Nope!");
-message.channel.bulkDelete(args).then(() => {
+message.channel.Delete(args[0]).then(() => {
   message.channel.send(`Cleared messages.`).then(msg => msg.delete(5000));
 });
 
